33425c2b003d012cba65004bbc59b9288aa95130
Soslan Dzgoev <dzgsoslan@gmail.com>
2023-05-16 16:30:14 +0400

    first commit

---
+++ src/main/java/com/example/myexpenses/MyExpensesApplication.java
@@ -0,0 +1,15 @@
+package com.example.myexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExpensesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyExpensesApplication.class, args);
+
+    }
+
+
+}
